c58c90d5310dbca83d9807cab28fa642c6d138c2
IlyaChistov <gaplis322@gmail.com>
2023-05-11 19:00:38 +0300

    Homework 2

---
+++ Homeworks/Homework_2/task_2.java
@@ -0,0 +1,11 @@
+// Если необходимо, исправьте данный код (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+
+try {
+    int d = 0;
+    double catchedRes1 = intArray[8] / d;
+    System.out.println("catchedRes1 = " + catchedRes1);
+ } catch (ArithmeticException e) {
+    System.out.println("Catching exception: " + e);
+ } catch (IndexOutOfBoundsException e) {
+    System.out.println("Catching exception: " + e);
+ }
